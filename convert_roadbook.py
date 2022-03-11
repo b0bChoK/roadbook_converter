e8394c448e08efee8cc06dd347d0d3ea8da45526
DEWAS Albert <b0b_ChoK@yahoo.fr>
2022-03-11 22:16:15 +0100

    Crop one case

--- convert_roadbook.py
+++ convert_roadbook.py
@@ -1,5 +1,6 @@
 #! python3
 import os
+from turtle import right
 from pdf2image import convert_from_path, convert_from_bytes
 
 from pdf2image.exceptions import (
@@ -18,12 +19,34 @@ def main():
 
     page_dir = "output/pages"
     case_dir = "output/cases"
-    os.makedirs(page_dir)
-    os.makedirs(case_dir)
+    try:
+        os.makedirs(page_dir)
+    except OSError as error:
+        pass
+    try:
+        os.makedirs(case_dir)
+    except OSError as error:
+        pass
+
+    left_margin = 34
+    right_margin = 1654 - 1608
+    left_column_width = 790 - 34
+    # right_column_left_border = 852 
+    right_column_width = 1654 - 852
+    top_margin = 238
+    bottom_margin = 2130
+    case_per_column = 8
+    case_height = (bottom_margin - top_margin) / case_per_column
 
     for i in range(len(images)):
         # Save pages as images in the pdf
         images[i].save('output/page'+ str(i) +'.jpg', 'JPEG')
+        print(images[i].size)
+
+        # crop case 1
+        case1 = images[i].crop((left_margin, top_margin, left_margin + left_column_width, top_margin + case_height))
+        case1.save('output/case'+ str(i) +'.jpg', 'JPEG')
+
 
 if __name__ == "__main__":
     main()
