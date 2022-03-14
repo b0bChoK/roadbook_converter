8d150893a57b4b1c98b94468d73369f08514c965
DEWAS Albert <b0b_ChoK@yahoo.fr>
2022-03-14 22:42:52 +0100

    [DEV] add config and crop all case

--- convert_roadbook.py
+++ convert_roadbook.py
@@ -1,5 +1,9 @@
 #! python3
+from ast import arg
 import os
+import argparse
+import configparser
+
 from turtle import right
 from pdf2image import convert_from_path, convert_from_bytes
 
@@ -13,39 +17,65 @@ from pdf2image.exceptions import (
 POPPLER_PATH = "poppler-22.01.0/Library/bin"
 
 def main():
-    print("Convert pdf")
+    parser = argparse.ArgumentParser(description='Convert FFM pdf roadbook to image, 1 image per case.')
+    parser.add_argument('files', metavar='N', type=str, nargs='+',
+                        help='files to convert')
+    parser.add_argument('-o', '--outdir', type=str, help='output directory where image will be stored')
+    parser.add_argument('-c', '--conf', type=str, default="page_config.toml", help='page configuration')
 
-    images = convert_from_path('pdf/test.pdf', poppler_path = POPPLER_PATH)
+    args = parser.parse_args()
 
-    page_dir = "output/pages"
-    case_dir = "output/cases"
-    try:
-        os.makedirs(page_dir)
-    except OSError as error:
-        pass
-    try:
-        os.makedirs(case_dir)
-    except OSError as error:
-        pass
+    outdir = args.outdir
 
-    left_margin = 34
-    right_margin = 1654 - 1608
-    left_column_width = 790 - 34
-    # right_column_left_border = 852 
-    right_column_width = 1654 - 852
-    top_margin = 238
-    bottom_margin = 2130
-    case_per_column = 8
-    case_height = (bottom_margin - top_margin) / case_per_column
+    config = configparser.ConfigParser()
+    config.readfp(open(args.conf))
 
-    for i in range(len(images)):
-        # Save pages as images in the pdf
-        images[i].save('output/page'+ str(i) +'.jpg', 'JPEG')
-        print(images[i].size)
+    page_width = config['DEFAULT']['page_width']
+    # page_width = 1654
+    left_margin = config['DEFAULT']['left_margin']
+    # left_margin = 34
+    right_margin = config['DEFAULT']['right_margin']
+    # right_margin = page_width - 1608
+    left_column_width = config['DEFAULT']['left_column_width']
+    # left_column_width = 790 - 34
+    # # right_column_left_border = 852 
+    right_column_width = config['DEFAULT']['right_column_width']
+    # right_column_width = page_width - 852
+    top_margin = config['DEFAULT']['top_margin']
+    # top_margin = 238
+    bottom_margin = config['DEFAULT']['bottom_margin']
+    # bottom_margin = 2130
+    case_per_column = config['DEFAULT']['case_per_column']
+    # case_per_column = 8
+    case_height = int((bottom_margin - top_margin) / case_per_column)
 
-        # crop case 1
-        case1 = images[i].crop((left_margin, top_margin, left_margin + left_column_width, top_margin + case_height))
-        case1.save('output/case'+ str(i) +'.jpg', 'JPEG')
+    for file in args.files:
+        print("Convert pdf", file)
+        images = convert_from_path(file, poppler_path = POPPLER_PATH)
+
+        case_dir = os.path.join(outdir, os.path.basename(file).split('.')[0])
+        try:
+            os.makedirs(case_dir)
+        except OSError as error:
+            pass
+        
+        cpt = 1
+        for i in range(len(images)):
+            # Save pages as images in the pdf
+            #images[i].save(os.path.join(case_dir, str(i) +'.jpg'), 'JPEG')
+            
+            # crop left column
+            for j in reversed(range(case_per_column)):
+                param = (left_margin, top_margin + (case_height * j), left_margin + left_column_width, top_margin + (case_height * (j+1)))
+                case = images[i].crop(param)
+                case.save(os.path.join(case_dir, "case_" + str(cpt) +'.jpg'), 'JPEG')
+                cpt+=1
+
+            # crop right column
+            for j in reversed(range(case_per_column)):
+                case = images[i].crop((page_width - right_column_width, top_margin + (case_height * j), page_width - right_margin, top_margin + (case_height * (j+1))))
+                case.save(os.path.join(case_dir, "case_" + str(cpt) +'.jpg'), 'JPEG')
+                cpt+=1
 
 
 if __name__ == "__main__":
